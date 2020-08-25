93c765f4a07d2d85fffb00718a420a14ff1e868d
Omer Eisenberg <omereis@yahoo.com>
2020-08-25 07:06:19 +0000

    implemented basic methods (copy constructor, operators)

--- psd_output.cpp
+++ psd_output.cpp
@@ -1,9 +1,46 @@
 
 #include "psd_output.h"
 
+/*
+template <class T>
+typedef vector < <vector<t> > 
+template <class T>
+bool CompareMatrix (
+*/
 TPsdOutput::TPsdOutput()
 {
-	m_mtxOut.clear ();
+	Clear ();
+}
+
+TPsdOutput::TPsdOutput(const TPsdOutput &other)
+{
+	AssignAll (other);
+}
+
+TPsdOutput TPsdOutput::operator= (const TPsdOutput &other)
+{
+	AssignAll (other);
+	return (*this);
+}
+
+void TPsdOutput::AssignAll (const TPsdOutput &other)
+{
+	SetParams(other.GetParams());
+	m_mtxOut = other.m_mtxOut;
+}
+
+bool TPsdOutput::operator== (const TPsdOutput &other) const
+{
+	if (GetParams() != other.GetParams())
+		return (false);
+	if (m_mtxOut != other.m_mtxOut)
+		return (false);
+	return (true);
+}
+
+bool TPsdOutput::operator!= (const TPsdOutput &other) const
+{
+	return (!(*this == other));
 }
 
 void TPsdOutput::AddSamples (float *afBuffer, uint32_t nSize)
@@ -21,4 +58,29 @@ uint32_t TPsdOutput::PulsesCount () const
 	return ((uint32_t) m_mtxOut.size());
 }
 
+void TPsdOutput::Clear ()
+{
+	TFloatMatrix::iterator imtx;
 
+	for (imtx = m_mtxOut.begin() ; imtx != m_mtxOut.end() ; imtx++)
+		imtx->clear ();
+	m_mtxOut.clear ();
+	m_params.Clear ();
+}
+
+void TPsdOutput::SetParams (const TPsdParams &params)
+{
+	m_params = params;
+}
+
+TPsdParams TPsdOutput::GetParams() const
+{
+	return (m_params);
+}
+
+void TPsdOutput::HandleNew(float *buff, uint32_t buff_size)
+{
+	if ((int) PulsesCount() < m_params.GetSaveRaw ()) {
+		AddSamples (buff, buff_size);
+	}
+}
--- psd_output.h
+++ psd_output.h
@@ -4,17 +4,29 @@
 #include <vector>
 using namespace std;
 #include "bimam_types.h"
+#include "psd_params.h"
+#include "psd_out_params.h"
 #include "redpitaya/rp.h"
 
 class TPsdOutput {
 public:
 	TPsdOutput();
-
+	TPsdOutput(const TPsdOutput &other);
+	TPsdOutput operator= (const TPsdOutput &other);
+	bool operator== (const TPsdOutput &other) const;
+	bool operator!= (const TPsdOutput &other) const;
+	void Clear ();
+	void SetParams (const TPsdParams &params);
+	TPsdParams GetParams() const;
 	void AddSamples (float *afBuffer, uint32_t nSize);
 	uint32_t PulsesCount () const;
+	void HandleNew(float *buff, uint32_t buff_size);
 protected:
+	void AssignAll (const TPsdOutput &params);
 private:
+	TPsdParams m_params;
 	TFloatMatrix m_mtxOut;
+	TPsdOutParamsVec vPsdParams;
 };
 #endif
 
