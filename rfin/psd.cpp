5f2c03458bad762410ef637a90301aec7292b548
username <username@mail.com>
2020-10-15 12:31:10 +0000

    measured dc

--- rfin/psd.cpp
+++ rfin/psd.cpp
@@ -10,6 +10,18 @@
 
 int main(int argc, char **argv)
 {
+	int nPrintSize = 10;
+
+	printf("argc=%d\n", argc);
+	if (argc > 1) {
+		try {
+			nPrintSize = atoi(argv[1]);
+			printf ("New print size\n");
+		}
+		catch(...) {
+			printf ("%s could not be converted to integer\n", argv[1]);
+		}
+	}
 	printf ("RF input reader\n");
 	system ("cat /opt/redpitaya/fpga/fpga_0.94.bit > /dev/xdevcfg");
 	if(rp_Init() != RP_OK){
@@ -18,6 +30,7 @@ int main(int argc, char **argv)
 
 	uint32_t buff_size = 16384;
 	float *buff = (float *)malloc(buff_size * sizeof(float));
+	uint16_t *auiBuffer = (uint16_t*) calloc(buff_size, sizeof(auiBuffer[0]));
 
 	rp_AcqReset();
 	rp_AcqSetDecimation(RP_DEC_1);
@@ -39,11 +52,13 @@ int main(int argc, char **argv)
 	}
 
 	rp_AcqGetOldestDataV(RP_CH_1, &buff_size, buff);
+	rp_AcqGetOldestDataRow(RP_CH_1, &buff_size, auiBuffer);
 	int i;
-	for(i = 0; i < 10; i++){
-		printf("%f\n", buff[i]);
+	for(i = 0; i < nPrintSize ; i++){
+		printf("%f,%d\n", buff[i], auiBuffer[i]);
 	}
 	free(buff);
+	free(auiBuff);
 	rp_Release();
 	return 0;
 }
--- rfin/rfin.c
+++ rfin/rfin.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #include "redpitaya/rp.h"
 #include "fpga.h"
 
@@ -12,6 +13,7 @@ int main(int argc, char **argv){
 
         /* Print error, if rp_Init() function failed */
 	printf ("RF input reader\n");
+
 /*
 	int nFpgaInit;
 	nFpgaInit = osc_fpga_init();
@@ -20,6 +22,7 @@ int main(int argc, char **argv){
 	printf ("FPGA Reset Code: %d\n", nFpgaInit);
 */
 	system ("cat /opt/redpitaya/fpga/fpga_0.94.bit > /dev/xdevcfg");
+	printf ("bit file copied\n");
         if(rp_Init() != RP_OK){
 
                 fprintf(stderr, "Rp api init failed!\n");
@@ -36,11 +39,13 @@ int main(int argc, char **argv){
 
         uint32_t buff_size = 16384;
         float *buff = (float *)malloc(buff_size * sizeof(float));
+		int16_t* auiBuffer = (int16_t*) calloc (buff_size, sizeof (auiBuffer[0]));
 
         rp_AcqReset();
         rp_AcqSetDecimation(RP_DEC_1);
         rp_AcqSetTriggerLevel(RP_CH_1, 10e-3); //Trig level is set in Volts while in SCPI
-		rp_AcqSetSamplingRate (RP_SMP_125M);
+		//rp_AcqSetSamplingRate (RP_SMP_125M);
+		rp_AcqSetSamplingRate (RP_SMP_15_625M);
 		
         rp_AcqSetTriggerDelay(0);
 
@@ -57,24 +62,57 @@ int main(int argc, char **argv){
         /*length and smaling rate*/
 
 	usleep(1);
-        rp_AcqSetTriggerSrc(RP_TRIG_SRC_CHA_PE);
-        rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
+	rp_AcqSetTriggerSrc(RP_TRIG_SRC_CHA_PE);
+	rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
+	short int fTrigger=0, fTimeout=0;
+	clock_t cStart;
+	//while(1){
+	cStart = clock();
+	double dDiff;
+	while((fTrigger == 0) && (fTimeout == 0)){
+	//	printf ("Preparing to read trigger\n");
+		rp_AcqGetTriggerState(&state);
+		//printf ("Read trigger\n");
+		if (state == RP_TRIG_STATE_TRIGGERED)//{
+			fTrigger = 1;
+		dDiff = (clock() - cStart);
+		dDiff = (double) CLOCKS_PER_SEC;
+		//printf ("Diff: %g\n", dDiff);
+		if (dDiff >= 3)
+		//if (((double) (clock() - cStart) / (double) CLOCKS_PER_SEC) >=2)
+			fTimeout = 1;
+		//else
+			printf ("Time Diff: %g\n", dDiff);
+			//break;
+		//}
+	}
+	if (fTrigger)
+		printf ("Triggered\n");
+	else
+		printf ("Timeout\n");
 
-        while(1){
-                rp_AcqGetTriggerState(&state);
-                if(state == RP_TRIG_STATE_TRIGGERED){
-                break;
-                }
-        }
-
-        rp_AcqGetOldestDataV(RP_CH_1, &buff_size, buff);
-        int i;
-	for(i = 0; i < 10; i++){
+	rp_AcqGetOldestDataV(RP_CH_1, &buff_size, buff);
+	uint32_t uiTriggerPos, uiLen=buff_size;//15000;
+	rp_AcqGetWritePointerAtTrig (&uiTriggerPos);
+	rp_AcqGetDataRaw (RP_CH_1, uiTriggerPos, &uiLen, auiBuffer);
+	//rp_AcqGetOldestDataRaw(RP_CH_1, &buff_size, auiBuffer);
+	int i, nPrintLen=buff_size;//15000;
+		FILE *file = stdout;
+		if (argc > 1) {
+			file = fopen (argv[1], "w+");
+			nPrintLen = buff_size;
+		}
+	for(i = 0; i < nPrintLen ; i++){
+	//for(i = 0; i < 1000; i++){
         //for(i = 0; i < buff_size; i++){
-                printf("%f\n", buff[i]);
+                //printf("%f\n", buff[i]);
+                fprintf(file, "%f,%d\n", buff[i], auiBuffer[i]);
         }
+		if (argc > 2)
+			fclose (file);
         /* Releasing resources */
         free(buff);
+		free (auiBuffer);
         rp_Release();
         return 0;
 }
